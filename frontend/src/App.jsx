12376e37dfb66fad9704bfa1bf0ab1fe2cd96f34
LukeFromAfar <lukasz.gamvik@gmail.com>
2025-03-10 08:21:03 +0100

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/authentication/Login";
+import Register from "./pages/authentication/Register";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="sign-in" element={<Login />} />
+        <Route path="sign-up" element={<Register />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import reactsvg from "../assets/react.svg";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/sign-in";
+
+  return (
+    <nav className="flex w-full justify-between p-4 bg-blue-300">
+      <Link to="/">
+        <img src={reactsvg} alt="Logo" className="w-8 h-8" />{" "}
+      </Link>
+      <h1 className="text-2xl font-bold">[Nettbutikk]</h1>
+      <Link
+        to={isLoginPage ? "/sign-up" : "/sign-in"}
+        className="text-btn btn-primary hover:text-blue-700" // Added hover effect
+      >
+        {isLoginPage ? "Register" : "Login"}
+      </Link>
+    </nav>
+  );
+}
